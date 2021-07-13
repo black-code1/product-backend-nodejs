23e0ac1469520e837e9f20c228c5b48afd83dab7
black-code1 <legrandmahamat1@gmail.com>
2021-07-13 07:24:36 +0100

    Ajout du midrésolvant le problème de CORS

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ const app = express();
   console.log('Réponse evoyée avec succès !');
 });*/
 
+// Problème de CORS
+app.use((req, res, next) => {
+  // d'accéder à notre API depuis n'importe quelle origine ( '*' ) 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) 
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+
+  //d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 //use : nous lui passons un string, correspondant à la route pour laquelle nous souhaitons enregistrer cet élément de middleware.
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
