36283b4c1fbf7cbd0faff929e951ab559d20da1b
black-code1 <legrandmahamat1@gmail.com>
2021-07-13 06:43:09 +0100

    Exécution de l'application Express sur le serveur Nod

---
+++ server.js
@@ -0,0 +1,9 @@
+// Creation d'un serveur Node
+const http = require('http');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
