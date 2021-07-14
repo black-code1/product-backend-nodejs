04e4f083260d6cc9a9871344bcfd9ee91b5cb397
black-code1 <legrandmahamat1@gmail.com>
2021-07-14 11:53:41 +0100

    Moved route from app.js to the route folder

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+
+app.use('/api/stuff', stuffRoutes);
 
 
 const dbURI = 'mongodb+srv://legrand:legrand@cluster0.jgecv.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(dbURI,{ useNewUrlParser: true,
-  useUnifiedTopology: true })
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then((resultat) => console.log('Connexion à MongoDB réussie !'))
   .catch((erreur) => console.log(erreur));
-  
-  const app = express();
+
+const app = express();
 
 
 
@@ -59,45 +63,6 @@ app.use((req, res, next) => {
 // définissez sa fonction json comme middleware global pour votre application,
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  
-  // Suppression _id envoyé par le front-end
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing
-      .updateOne({_id : req.params.id}, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-      .catch(error => res.status(400).json({ error }));
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing
-      .deleteOne({_id : req.params.id})
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-})
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing
-      .findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-})
-//use : nous lui passons un string, correspondant à la route pour laquelle nous souhaitons enregistrer cet élément de middleware.
-app.get('/api/stuff', (req, res, next) => {
-  Thing
-      .find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-});
 
 
 module.exports = app;
